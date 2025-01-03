dd02f34affd393ab3b35326c39baecfab1a378c7
Chris Myers <c486733@cttlc.com>
2025-01-03 12:31:02 -0500

    Added embeddings model

--- deployments/infra/terraform/ai_models.tf
+++ deployments/infra/terraform/ai_models.tf
@@ -10,8 +10,26 @@ resource "azurerm_cognitive_deployment" "gpt_4o_mini" {
 
   sku {
     name = "GlobalStandard"
-capacity = 100
+    capacity = 100
   }
 
   rai_policy_name = "Microsoft.DefaultV2"
+}
+
+resource "azurerm_cognitive_deployment" "text_embeddings" {
+  name                 = "text-embedding-ada-002"
+  cognitive_account_id = azurerm_ai_services.this.id
+
+  model {
+    format  = "OpenAI"
+    name    = "text-embedding-ada-002"
+    version = "2"
+  }
+
+  sku {
+    name = "Standard"
+    capacity = 100
+  }
+
+  rai_policy_name = "Microsoft.Default"
 }
