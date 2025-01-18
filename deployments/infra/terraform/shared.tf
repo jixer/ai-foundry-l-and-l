b06a7981b84f76aa47bafc1da313ded3a7a10458
Chris Myers <c486733@cttlc.com>
2025-01-17 23:45:34 -0500

    LRS and RBAC

--- deployments/infra/terraform/shared.tf
+++ deployments/infra/terraform/shared.tf
@@ -4,18 +4,19 @@ resource "azurerm_storage_account" "default" {
   location                        = var.location
   resource_group_name             = azurerm_resource_group.foundry.name
   account_tier                    = "Standard"
-  account_replication_type        = "GRS"
+  account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
 }
 
 // KEY VAULT
 resource "azurerm_key_vault" "default" {
-  name                     = local.names.foundry_kv
-  location                 = var.location
-  resource_group_name      = azurerm_resource_group.foundry.name
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  sku_name                 = "standard"
-  purge_protection_enabled = false
+  name                      = local.names.foundry_kv
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.foundry.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  purge_protection_enabled  = false
+  enable_rbac_authorization = true
 }
 
 
